6ea59fc6be8885a2b5836485bef827a5f40d6793
JinGyeong <wlsrud677@naver.com>
2023-07-28 17:48:57 +0900

    feat : 3pl service 입고 등록 조회

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLService.java
@@ -2,7 +2,9 @@ package com.shinhan.sunInCloud.service;
 
 import org.springframework.stereotype.Service;
 
+import com.shinhan.sunInCloud.entity.ImportProduct;
 import com.shinhan.sunInCloud.entity.ThreePL;
+import com.shinhan.sunInCloud.repository.SellerRepository;
 import com.shinhan.sunInCloud.repository.ThreePLRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class ThreePLService {
 
 	private final ThreePLRepository threePLRepository;
-	
+	private final SellerRepository sellerRepo;
 	/**
 	 * 3PL 등록 메소드
 	 * @param threePL
@@ -20,5 +22,11 @@ public class ThreePLService {
 	 */
 	public ThreePL register(ThreePL threePL) {
 		return threePLRepository.save(threePL);
+	}
+	
+	
+	//입고등록에서 입고 등록 조회
+	public ImportProduct selectImportProduct(List<Product> product) {
+		return threePLRepository.findBy
 	}
 }
