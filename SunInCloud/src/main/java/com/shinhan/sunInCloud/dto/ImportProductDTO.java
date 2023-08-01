91e1876199f472fc286ca87ad327e5fea6cdd3ca
JinGyeong <wlsrud677@naver.com>
2023-08-01 17:13:33 +0900

    feat : 입고 상품 DTO 생성

---
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/ImportProductDTO.java
@@ -0,0 +1,10 @@
+package com.shinhan.sunInCloud.dto;
+
+public class ImportProductDTO {
+	private Long importProductNo;
+	private String productNo;
+	private Long importNo;
+	private int requestAmount;
+	private int importAmount;
+
+}
