593a695e5f49701c2d3f6e85f6be774a2a758c36
junbeomSon <junbeom247@gmail.com>
2023-08-01 11:30:47 +0900

    fix : 세부 상품군 데이터 여러개 저장

--- SunInCloud/src/test/java/com/shinhan/sunInCloud/detailProductGroup/DetailProductGroupTest.java
+++ SunInCloud/src/test/java/com/shinhan/sunInCloud/detailProductGroup/DetailProductGroupTest.java
@@ -20,16 +20,18 @@ public class DetailProductGroupTest {
 	@Test
 	void registerDetailProductGroup() {
 		ProductGroup productGroup = productGroupService.findByGroupName("화장품");
-		DetailProductGroup detailProductGroup = DetailProductGroup
-				.builder()
-				.groupName("크림")
-				.productGroup(productGroup)
-				.build();
-		
-		DetailProductGroup savedDetailProductGroup = detailProductGroupService.register(detailProductGroup);
-		
-		Assertions.assertThat(savedDetailProductGroup.getDetailProductGroupNo()).isNotNull();
-		Assertions.assertThat(savedDetailProductGroup.getGroupName()).isEqualTo(detailProductGroup.getGroupName());
+		String[] detailGroupNames = {"메이크업", "네일", "클렌징", "스킨", "크림"};
+		for (String detailGroupName : detailGroupNames) {
+			DetailProductGroup detailProductGroup = DetailProductGroup
+					.builder()
+					.groupName(detailGroupName)
+					.productGroup(productGroup)
+					.build();
+
+			DetailProductGroup savedDetailProductGroup = detailProductGroupService.register(detailProductGroup);
+			Assertions.assertThat(savedDetailProductGroup.getDetailProductGroupNo()).isNotNull();
+			Assertions.assertThat(savedDetailProductGroup.getGroupName()).isEqualTo(detailProductGroup.getGroupName());
+		}
 	}
 	
 }
