c5b24e6b1556fab697dd85560d9fcdf520fa8125
JinGyeong <wlsrud677@naver.com>
2023-08-01 17:13:17 +0900

    feat : 입고DTO 생성

---
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/ImportsDTO.java
@@ -0,0 +1,11 @@
+package com.shinhan.sunInCloud.dto;
+
+import java.sql.Timestamp;
+
+public class ImportsDTO {
+	private Long importNo;
+	private Long sellerNo;
+	private Timestamp requestDate;
+	private Timestamp importDate;
+
+}
