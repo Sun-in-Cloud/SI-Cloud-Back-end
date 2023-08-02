34df1e2b5faa09ffffb5c85c1ca457f2e4c71d24
JinGyeong <wlsrud677@naver.com>
2023-08-02 14:16:50 +0900

    feat : dto entity 변환

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/ImportsDTO.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/ImportsDTO.java
@@ -2,10 +2,22 @@ package com.shinhan.sunInCloud.dto;
 
 import java.sql.Timestamp;
 
+import com.shinhan.sunInCloud.entity.Imports;
+import com.shinhan.sunInCloud.entity.Seller;
+
 public class ImportsDTO {
 	private Long importNo;
 	private Long sellerNo;
 	private Timestamp requestDate;
 	private Timestamp importDate;
-
+	
+	public Imports toImport(Seller seller) {
+		return Imports.builder()
+				.importNo(importNo)
+				.seller(seller)
+				.importDate(importDate)
+				.importNo(importNo)
+				.requestDate(requestDate)
+				.build();
+	}
 }
