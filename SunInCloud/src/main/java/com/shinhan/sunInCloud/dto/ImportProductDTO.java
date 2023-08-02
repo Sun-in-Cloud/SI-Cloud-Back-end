9228e401d8fd1b2c8c8e84f963b1ce578ae06fd5
JinGyeong <wlsrud677@naver.com>
2023-08-02 17:06:15 +0900

    feat : 입고 내역 entity->dto 메서드 추가

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/ImportProductDTO.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/ImportProductDTO.java
@@ -1,9 +1,26 @@
 package com.shinhan.sunInCloud.dto;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+
 import com.shinhan.sunInCloud.entity.ImportProduct;
 import com.shinhan.sunInCloud.entity.Imports;
 import com.shinhan.sunInCloud.entity.Product;
+import com.shinhan.sunInCloud.entity.Seller;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class ImportProductDTO {
 	private Long importProductNo;
 	private String productNo;
