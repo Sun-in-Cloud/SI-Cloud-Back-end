9767a216d4007a8fa6698866885999066bea17de
Eunbeen Oh <svbean77@naver.com>
2023-08-02 12:09:24 +0900

    fix : export, shopping repository 쿼리 추가

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/ExportProductRepository.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/ExportProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.shinhan.sunInCloud.entity.ExportProduct;
 
 public interface ExportProductRepository extends JpaRepository<ExportProduct, Long>{
-
+	Long countByExports_ExportNoAndOrderStatus(String exportNo, String orderStatus);
 }
