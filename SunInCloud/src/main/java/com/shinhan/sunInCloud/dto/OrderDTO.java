ba8941c9f0c61e8cd1d67443adea5852680cbd0c
junbeomSon <junbeom247@gmail.com>
2023-08-02 15:51:10 +0900

    feat : OrderDTO 생성

---
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/dto/OrderDTO.java
@@ -0,0 +1,20 @@
+package com.shinhan.sunInCloud.dto;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class OrderDTO {
+
+	private Long orderNo;
+	private Timestamp orderDate;
+}
