71cccb0357f5696bbe65782371fd921d0a1f073f
JinGyeong <wlsrud677@naver.com>
2023-08-02 14:15:56 +0900

    feat : 창고 서비스 한글 로직 작성

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLImportService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLImportService.java
@@ -1,5 +1,30 @@
 package com.shinhan.sunInCloud.service;
 
-public class ThreePLImportService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.shinhan.sunInCloud.dto.ImportsDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ThreePLImportService {
+	
+	//1.입고예정리스트 -목록 조회
+//	private List<ImportsDTO> seePrelist(Long sellerNo, int pageNum, int countPerPage){
+//		//return importRepo.findImports_
+//	}
+	
+	//2.입고예정리스트 - 상세 조회
+	
+	//3.입고 - 등록 패이지로 이동
+	
+	//4.입고 - 등록
+	
+	//5.입고내역 리스트 -목록 조회
+	
+	//6.입고내역리스트 - 상세 조회
+	
 }
