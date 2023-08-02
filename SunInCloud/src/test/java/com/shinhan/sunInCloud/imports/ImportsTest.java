b162997a89c14fb85653be09142eee1a9745d7c3
JinGyeong <wlsrud677@naver.com>
2023-08-02 14:15:25 +0900

    test : 입고 테스트 진행중

--- SunInCloud/src/test/java/com/shinhan/sunInCloud/imports/ImportsTest.java
+++ SunInCloud/src/test/java/com/shinhan/sunInCloud/imports/ImportsTest.java
@@ -1,8 +1,14 @@
 package com.shinhan.sunInCloud.imports;
 
+import java.util.Date;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import com.shinhan.sunInCloud.entity.Imports;
 import com.shinhan.sunInCloud.service.SellerImportService;
 
 @SpringBootTest
@@ -12,4 +18,12 @@ public class ImportsTest {
 	SellerImportService importService;
 	
 	
+	@Test
+	void seePreList() {
+		List<Imports> imports = new ArrayList<>();
+		Timestamp timeStamp = new Timestamp(new Date().getTime());
+		Imports im = Imports.builder().requestDate(timeStamp)
+				.importDate(timeStamp).build();
+		
+	}
 }
