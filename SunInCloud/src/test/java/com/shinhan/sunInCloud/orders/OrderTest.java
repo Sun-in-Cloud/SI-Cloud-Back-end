eaa3182ce755307ea43240b666186269e66d5ac3
junbeomSon <junbeom247@gmail.com>
2023-08-02 12:33:06 +0900

    test : 발주 등록 테스트

--- SunInCloud/src/test/java/com/shinhan/sunInCloud/orders/OrderTest.java
+++ SunInCloud/src/test/java/com/shinhan/sunInCloud/orders/OrderTest.java
@@ -8,13 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.shinhan.sunInCloud.dto.OrderProductDTO;
+import com.shinhan.sunInCloud.entity.Product;
 import com.shinhan.sunInCloud.service.OrderService;
+import com.shinhan.sunInCloud.service.ProductService;
 
 @SpringBootTest
 public class OrderTest {
 
 	@Autowired
 	OrderService orderService;
+	
+	@Autowired
+	ProductService productService;
 	
 	@Test
 	void findNeededOrderProducts() {
@@ -26,5 +31,14 @@ public class OrderTest {
 			System.out.println(orderProduct);
 			Assertions.assertThat(orderProduct.getAmount()).isEqualTo(orderProduct.getEnoughStock() - orderProduct.getCurrentStock());
 		}
+	}
+	
+	@Test
+	void registerOrder() {
+		Long sellerNo = 8L;
+		boolean registered = orderService.register(sellerNo);
+		Assertions.assertThat(registered).isTrue();
+		List<Product> neededToOrderProducts = productService.findNeededToOrderBySellerNo(sellerNo);
+		Assertions.assertThat(neededToOrderProducts.size()).isEqualTo(0);
 	}
 }
