2a4156b1f6f4dfc5c172fc7e83d08525c3642cf7
Eunbeen Oh <svbean77@naver.com>
2023-08-02 12:44:17 +0900

    feat : shopping 주문 생성 컨트롤러

---
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/controller/ShoppingController.java
@@ -0,0 +1,24 @@
+package com.shinhan.sunInCloud.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shinhan.sunInCloud.dto.ShoppingDTO;
+import com.shinhan.sunInCloud.dto.ShoppingProductDTO;
+import com.shinhan.sunInCloud.service.ShoppingService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ShoppingController {
+	private final ShoppingService shoppingService;
+	
+	@PostMapping("/shop/order")
+	public boolean register(@RequestBody ShoppingDTO shopping) {
+		return shoppingService.register(shopping.getSellerNo(), shopping.getOrderedProducts());
+	}
+}
