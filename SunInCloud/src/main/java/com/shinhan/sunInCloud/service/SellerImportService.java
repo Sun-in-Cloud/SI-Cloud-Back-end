f86a144ded98b731ed969f515a0be2c67ccabe9b
JinGyeong <wlsrud677@naver.com>
2023-08-03 11:33:04 +0900

    fix : sellerNo의 발주 목록 조회 메서드 수정

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/SellerImportService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/SellerImportService.java
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Service;
 
 import com.shinhan.sunInCloud.dto.ImportProductDTO;
 import com.shinhan.sunInCloud.dto.ImportsDTO;
+import com.shinhan.sunInCloud.dto.OrderDTO;
+import com.shinhan.sunInCloud.dto.OrderProductDTO;
 import com.shinhan.sunInCloud.entity.ImportProduct;
 import com.shinhan.sunInCloud.entity.Imports;
 import com.shinhan.sunInCloud.entity.Order;
+import com.shinhan.sunInCloud.entity.OrderProduct;
 import com.shinhan.sunInCloud.entity.Seller;
 import com.shinhan.sunInCloud.repository.ImportsProductRepository;
 import com.shinhan.sunInCloud.repository.ImportsRepository;
+import com.shinhan.sunInCloud.repository.OrderProductRepository;
 import com.shinhan.sunInCloud.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -29,18 +33,38 @@ public class SellerImportService {
 	private final ImportsRepository importRepository;
 	private final ImportsProductRepository importProductRepository;
 	private final OrderRepository orderRepository;
-
+	private final OrderProductRepository orderProductRepository;
 	
-	//1.입고 예정 리스트 등록
-	//1.1 발주 조회->목록
-	public Optional<Order> findByOrderNo(Long orderNo){
-		return orderRepository.findById(orderNo);
+	/**
+	 * 1.입고 예정 리스트 등록
+	 * 1.1 발주 조회->목록
+	 * @param sellerNo
+	 * @return orders의 발주번호, 발주일자
+	 */
+	public Optional<Order> findBySellerNo(String sellerNo){
+		return orderRepository.findBySellerNo(sellerNo);
 	}
+	
 		
-		//1.2 발주 조회->상세
-//		public Order findByOrderProductNo(Long orderProductNo) {
-//			return orderRepo.findByOrderProductNo(orderProductNo);
-//		}
+	/**
+	 * //1.2 발주 번호로 상세 조회
+	 * @param orderNo
+	 * @return 바코드 번호, 상품 이름, 발주 수량
+	 */
+		public List<OrderProductDTO> findByProductNo(String orderNo) {
+			List<OrderProductDTO> orderDTOs = new ArrayList<>();
+			List<OrderProduct> order = orderProductRepository.findByOrderNo(orderNo);
+			//entity를 dto로 변환하기	
+			for(OrderProduct orders: order) {
+				orderDTOs.add(OrderProductDTO.builder().productNo(orders.getProduct().getProductNo())
+						.amount(orders.getAmount())
+						.productName(orders.getProduct().getProductName())
+						.build());
+			}
+			
+			return orderDTOs;
+			
+		}
 	
 		//1.3 발주 조회->등록
 		//public Product saveProduct(Product ) {
@@ -52,43 +76,43 @@ public class SellerImportService {
 //			return orderRepository.saveAll(importNo);
 //		}
 //		
-		//3.입고 예정 리스트
-		//3.1 입고 예정 리스트 목록
-		public List<ImportsDTO> seePreList(ImportsDTO importDTO, Long sellerNo, int pageNumber, int pageSize) {
-			//dto로 변환해줘서 반환
-			List<ImportsDTO> importsDTOs = new ArrayList<>();
-			Page<Imports> page = importRepository.findBySellerNo(sellerNo, PageRequest.of(pageNumber, pageSize));
-			for(Imports imports : page) {
-				importsDTOs.add(importDTO.builder().importDate(imports.getImportDate())
-						.importNo(imports.getImportNo())
-						.requestDate(imports.getRequestDate())
-						.sellerNo(imports.getSeller().getSellerNo())
-						.build());
-			}	
-			return importsDTOs;
-		}
-		
-		//3.2 입고 예정 리스트 상세->입고 등록 후 입고가 확정된 상태이므로 실제 입고 수량을 보는것임!
-		public List<ImportProductDTO> seePreDetail(ImportProductDTO importProductDTO, Long importNo) {
-			List<ImportProductDTO> importsProductDTOs=new ArrayList<>();
-			List<ImportProduct> im= importProductRepository.findByImportNo(importNo);
-			for(ImportProduct imp: im) {
-				importsProductDTOs.add(importProductDTO.builder()
-						.productNo(imp.getProduct().getProductNo())
-						.productName(imp.getProduct().getProductName())
-						.importAmount(imp.getImportAmount())
-						.build());
-			}
-			return importsProductDTOs;
-		}
-		
-		//4.입고 내역 리스트
-		//4.1 목록
-		public List<ImportsDTO> seeList(Long sellerNo, int pageNum, int countPerPage){
-			Page<Imports> imports=importRepository.findBySellerNo(sellerNo, PageRequest.of(pageNum, countPerPage));
-			List<ImportsDTO> importDTO =new ArrayList<>();
-			return importDTO;
-		}
+//		//3.입고 예정 리스트
+//		//3.1 입고 예정 리스트 목록
+//		public List<ImportsDTO> seePreList(ImportsDTO importDTO, Long sellerNo, int pageNumber, int pageSize) {
+//			//dto로 변환해줘서 반환
+//			List<ImportsDTO> importsDTOs = new ArrayList<>();
+//			Page<Imports> page = importRepository.findBySellerNo(sellerNo, PageRequest.of(pageNumber, pageSize));
+//			for(Imports imports : page) {
+//				importsDTOs.add(importDTO.builder().importDate(imports.getImportDate())
+//						.importNo(imports.getImportNo())
+//						.requestDate(imports.getRequestDate())
+//						.sellerNo(imports.getSeller().getSellerNo())
+//						.build());
+//			}	
+//			return importsDTOs;
+//		}
+//		
+//		//3.2 입고 예정 리스트 상세->입고 등록 후 입고가 확정된 상태이므로 실제 입고 수량을 보는것임!
+//		public List<ImportProductDTO> seePreDetail(ImportProductDTO importProductDTO, Long importNo) {
+//			List<ImportProductDTO> importsProductDTOs=new ArrayList<>();
+//			List<ImportProduct> im= importProductRepository.findByImportNo(importNo);
+//			for(ImportProduct imp: im) {
+//				importsProductDTOs.add(importProductDTO.builder()
+//						.productNo(imp.getProduct().getProductNo())
+//						.productName(imp.getProduct().getProductName())
+//						.importAmount(imp.getImportAmount())
+//						.build());
+//			}
+//			return importsProductDTOs;
+//		}
+//		
+//		//4.입고 내역 리스트
+//		//4.1 목록
+//		public List<ImportsDTO> seeList(Long sellerNo, int pageNum, int countPerPage){
+//			Page<Imports> imports=importRepository.findBySellerNo(sellerNo, PageRequest.of(pageNum, countPerPage));
+//			List<ImportsDTO> importDTO =new ArrayList<>();
+//			return importDTO;
+//		}
 		
 		//4.2 상세
 //		public List<ImportsDTO> seeDetail(Long importNo){
