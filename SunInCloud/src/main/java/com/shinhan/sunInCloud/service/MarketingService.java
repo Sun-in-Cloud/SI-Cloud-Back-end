9bcb445ad1f165b224d055acb50757a228243dc0
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-08-04 10:42:41 +0900

    feat : 마케팅 서비스 생성

---
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/MarketingService.java
@@ -0,0 +1,11 @@
+package com.shinhan.sunInCloud.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MarketingService {
+
+}
