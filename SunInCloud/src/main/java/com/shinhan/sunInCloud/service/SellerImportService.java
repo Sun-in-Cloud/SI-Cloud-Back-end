d5f8492c8ca04ec19c2f72a1dd515032dd34d0cf
JinGyeong <wlsrud677@naver.com>
2023-08-04 14:06:01 +0900

    feat : service에 입고 예정 리스트 조회 메서드 진행중

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/SellerImportService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/SellerImportService.java
@@ -3,9 +3,15 @@ package com.shinhan.sunInCloud.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.shinhan.sunInCloud.dto.ImportProductDTO;
+import com.shinhan.sunInCloud.dto.ImportProductListDTO;
+import com.shinhan.sunInCloud.dto.ImportsDTO;
+import com.shinhan.sunInCloud.dto.OrderDTO;
+import com.shinhan.sunInCloud.dto.OrderListDTO;
 import com.shinhan.sunInCloud.entity.ImportProduct;
 import com.shinhan.sunInCloud.entity.Imports;
 import com.shinhan.sunInCloud.entity.Order;
@@ -39,10 +45,6 @@ public class SellerImportService {
 	 * @param sellerNo
 	 * @return orders의 발주번호, 발주일자
 	 */
-	public List<Order> findBySellerNo(Long sellerNo){
-		return orderRepository.findBySeller_SellerNo(sellerNo);
-	}
-	
 		
 	/**
 	 * //1.2 발주 번호로 상세 조회
@@ -104,20 +106,29 @@ public class SellerImportService {
 	         return imports.getImportNo() != null;
 	      }
 
-//		
-//		//3.입고 예정 리스트
-//		//3.1 입고 예정 리스트 목록
-//		public List<ImportsDTO> seePreList(ImportsDTO importDTO, Long sellerNo, int pageNumber, int pageSize) {
+		/**3.입고 예정 리스트
+		 * 3.1 입고 예정 리스트 목록 조회
+		 * @param importDTO
+		 * @param sellerNo
+		 * @param pageNumber
+		 * @param pageSize
+		 * @return
+		 */
+//		public List<ImportsDTO> seePreList(Long sellerNo, int pageNumber, int countPerPage) {
 //			//dto로 변환해줘서 반환
 //			List<ImportsDTO> importsDTOs = new ArrayList<>();
-//			Page<Imports> page = importRepository.findBySellerNo(sellerNo, PageRequest.of(pageNumber, pageSize));
-//			for(Imports imports : page) {
-//				importsDTOs.add(importDTO.builder().importDate(imports.getImportDate())
+//			Page<Imports> im = importRepository.findBySeller_SellerNo(sellerNo, PageRequest.of(pageNumber, countPerPage));
+//			for(Imports imports : im) {
+//				importsDTOs.add(ImportsDTO.builder().importDate(imports.getImportDate())
 //						.importNo(imports.getImportNo())
 //						.requestDate(imports.getRequestDate())
 //						.sellerNo(imports.getSeller().getSellerNo())
 //						.build());
 //			}	
+//			
+//			Long count = orderRepository.countBySeller_SellerNo(sellerNo);
+//			Long totalPage = calculatePageCount(count, countPerPage);
+//			ImportProductListDTO.builder().totalPage(totalPage).products(products).build();
 //			return importsDTOs;
 //		}
 //		
@@ -149,6 +160,14 @@ public class SellerImportService {
 //			imports = importRepository.findByImportNo(importNo);
 //			return imports;	
 //		}
+		
+		private Long calculatePageCount(Long count, int countPerPage) {
+			Long totalPage = count / countPerPage;
+			if (count % countPerPage > 0) {
+				++totalPage;
+			}
+			return totalPage;
+		}
 
 }
 
