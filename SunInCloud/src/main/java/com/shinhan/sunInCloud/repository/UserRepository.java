18dd67190e4d24a28fd38f942dec5b8126ebba6f
Eunbeen Oh <svbean77@naver.com>
2023-08-04 16:03:04 +0900

    feat : UserRepository 생성

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/UserRepository.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.shinhan.sunInCloud.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shinhan.sunInCloud.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
