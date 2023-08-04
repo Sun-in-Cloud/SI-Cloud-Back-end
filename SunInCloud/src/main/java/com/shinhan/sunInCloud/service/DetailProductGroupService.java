f8be77e54af40e3ff4193717d945cd31897feb79
Eunbeen Oh <svbean77@naver.com>
2023-08-04 16:34:37 +0900

    feat : 상품군, 세부 상품군 목록 조회 repository, service

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/DetailProductGroupService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/DetailProductGroupService.java
@@ -1,8 +1,13 @@
 package com.shinhan.sunInCloud.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.shinhan.sunInCloud.dto.ProductGroupDTO;
 import com.shinhan.sunInCloud.entity.DetailProductGroup;
+import com.shinhan.sunInCloud.entity.Seller;
 import com.shinhan.sunInCloud.repository.DetailProductGroupRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DetailProductGroupService {
 	private final DetailProductGroupRepository detailProductGroupRepository;
+	private final SellerService sellerService;
 
 	/**
 	 * 새로운 DetailProductGroup을 등록하는 메서드
@@ -30,5 +36,25 @@ public class DetailProductGroupService {
 	 */
 	public DetailProductGroup findByGroupName(String groupName) {
 		return detailProductGroupRepository.findByGroupName(groupName);
+	}
+	
+	/**
+	 * 화주사의 상품군에 해당하는 세부 상품군 리스트 조회
+	 * @param sellerNo
+	 * @return
+	 */
+	public List<ProductGroupDTO> findByProductGroup(Long sellerNo) {
+		List<ProductGroupDTO> productGroupDTOs = new ArrayList<>();
+		Seller seller = sellerService.findById(sellerNo);
+		
+		List<DetailProductGroup> detailProductGroups = detailProductGroupRepository
+				.findByProductGroup_GroupName(seller.getProductGroup().getGroupName());
+		
+		for(DetailProductGroup detailProductGroup: detailProductGroups) {
+			ProductGroupDTO productGroupDTO = ProductGroupDTO.builder().groupName(detailProductGroup.getGroupName()).build();
+			productGroupDTOs.add(productGroupDTO);
+		}
+		
+		return productGroupDTOs;
 	}
 }
