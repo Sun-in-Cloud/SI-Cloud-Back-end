78dcc5078d74358294c966a9fbb9029a4d89d2e4
JinGyeong <wlsrud677@naver.com>
2023-08-04 14:09:00 +0900

    fix : 입고 예정, 내역 리스트 목록 sellerNo로 찾기 return type page로 수정

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/ImportsRepository.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/ImportsRepository.java
@@ -3,6 +3,7 @@ package com.shinhan.sunInCloud.repository;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.shinhan.sunInCloud.entity.Imports;
@@ -15,8 +16,6 @@ public interface ImportsRepository extends JpaRepository<Imports, Long> {
 	//2. 입고 예정, 내역 리스트 목록 sellerNo로 찾기
 	Page<Imports> findBySeller_SellerNo(Long sellerNo, Pageable pageables);
 
-	//입고 등록을 위해 입고 번호 가져오기
-	Imports findByImportProduct_ImportNo(Long sellerNo);
 	
 	//4.리턴값 List이고, Id로 입고 상세 내역 찾기
 	//List<Imports> findByImportNo(Long importNo);
