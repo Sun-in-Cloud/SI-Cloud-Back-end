38df413e32e0f090dcf05d1459e276d10dc7db98
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-08-04 14:47:55 +0900

    fix : ExportsRepository -> ExportsProductRepositroy로 메소드 이동

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/ExportProductRepository.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/repository/ExportProductRepository.java
@@ -1,8 +1,13 @@
 package com.shinhan.sunInCloud.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shinhan.sunInCloud.entity.ExportProduct;
 
@@ -10,4 +15,15 @@ public interface ExportProductRepository extends JpaRepository<ExportProduct, Lo
 	Long countByExports_ExportNoAndOrderStatus(String exportNo, String orderStatus);
 	Page<ExportProduct> findByExports_ExportNoOrderByProduct_ProductName(String exportNo, Pageable pageable);
 	ExportProduct findByExports_ExportNoAndProduct_ProductNo(String exportNo, String productNo);
+	
+	@Query(value = "SELECT Date(e.export_date) as exportDate, COUNT(DISTINCT export_no) as exportCount "
+			+ "FROM export_product e "
+			+ "WHERE export_no in ( "
+			+ "SELECT export_no "
+			+ "FROM exports "
+			+ "WHERE seller_seller_no = :sellerNo ) "
+			+ " AND "
+			+ "export_date IS NOT NULL AND Date(e.export_date) BETWEEN :startDate AND :endDate "
+			+ "GROUP BY Date(e.export_date)", nativeQuery = true)
+	List<Object[]> getDailySalesCountForWeek(@Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("sellerNo") Long sellerNo);
 }
