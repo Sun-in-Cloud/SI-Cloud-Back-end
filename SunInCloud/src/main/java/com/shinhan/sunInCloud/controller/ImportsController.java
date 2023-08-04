549b64b0596ff621b168ded2786d12c385736cec
JinGyeong <wlsrud677@naver.com>
2023-08-04 17:15:57 +0900

    feat : 입고 예정 리스트 상세 조회 컨트롤 메서드 생성

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/controller/ImportsController.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/controller/ImportsController.java
@@ -3,7 +3,9 @@ package com.shinhan.sunInCloud.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shinhan.sunInCloud.dto.ImportProductDTO;
@@ -30,4 +32,18 @@ public class ImportsController {
 	public ImportProductListDTO seePreList (Long sellerNo, int pageNumber, int countPerPage){
 		return sellerImportService.seePreList(sellerNo, pageNumber, countPerPage);
 	}
+	
+	//입고예정 리스트 상세 조회
+	@GetMapping(value= {"/seller/import/pre/{importNo}"})
+	public List<ImportProductDTO> seePreDetail(ImportProductDTO importProductDTO, @PathVariable Long importNo) {
+		return sellerImportService.seePreDetail(importProductDTO, importNo);
+	}
+	
 }
+
+
+
+
+
+
+
