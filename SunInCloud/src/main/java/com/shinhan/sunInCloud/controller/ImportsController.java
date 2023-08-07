8fef630f9d1e86bb498902eaa1c79d7bbc3656f6
JinGyeong <wlsrud677@naver.com>
2023-08-07 14:57:52 +0900

    fix : 발주 상세 조회 수정

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/controller/ImportsController.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/controller/ImportsController.java
@@ -35,10 +35,10 @@ public class ImportsController {
 	}
 	
 	//입고예정 리스트 상세 조회
-	@GetMapping(value= {"/seller/import/pre/{importNo}"})
-	public ImportProductDTO seePreDetail(ImportProductDTO importProductDTO, @PathVariable Long importNo) {
-		return sellerImportService.seePreDetail(importProductDTO, importNo);
-	}
+//	@GetMapping(value= {"/seller/import/pre/{importNo}"})
+//	public ImportProductDTO seePreDetail(ImportProductDTO importProductDTO, @PathVariable Long importNo) {
+//		return sellerImportService.seePreDetail(importProductDTO, importNo);
+//	}
 	//발주 상품 검색
 	@GetMapping(value= {"/seller/import/search"})
 	public List<Product> searchOrder(String productName) {
