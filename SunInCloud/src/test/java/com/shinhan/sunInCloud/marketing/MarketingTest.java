23120a3580430068c17e8b6a95dd35a557817181
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-08-07 11:16:16 +0900

    test : 당월, 전월, 작년 동월 판매 건수 통계 테스트

---
+++ SunInCloud/src/test/java/com/shinhan/sunInCloud/marketing/MarketingTest.java
@@ -0,0 +1,43 @@
+package com.shinhan.sunInCloud.marketing;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.shinhan.sunInCloud.dto.NumberOfSalesDTO;
+import com.shinhan.sunInCloud.dto.StatisticsDTO;
+import com.shinhan.sunInCloud.service.MarketingService;
+
+@SpringBootTest
+public class MarketingTest {
+	
+	@Autowired
+	MarketingService marketingService;
+
+	@Test
+	void findStatisticsBySeller() {
+		StatisticsDTO statistics = marketingService.getStatisticsBySeller(8L);
+		List<NumberOfSalesDTO> numberOfSalesMonthly = statistics.getNumberOfSalesMonthly();
+		getNumberOfSalesMonthlyTest(numberOfSalesMonthly);
+	}
+	
+	void getNumberOfSalesMonthlyTest(List<NumberOfSalesDTO> numberOfSalesMonthly) {
+		for (NumberOfSalesDTO numberOfSales : numberOfSalesMonthly) {
+			int year = numberOfSales.getYear(); 
+			int month = numberOfSales.getMonth();
+			Long count = numberOfSales.getNumberOfSales();
+			if (year == 2023) { // 당월, 전월
+				if (month == 8) { // 당월
+					Assertions.assertThat(count).isEqualTo(5L);
+				} else { // 전월
+					Assertions.assertThat(count).isEqualTo(0L);
+				}
+			} else {
+				Assertions.assertThat(count).isEqualTo(0L);
+			}
+		}
+	}
+}
