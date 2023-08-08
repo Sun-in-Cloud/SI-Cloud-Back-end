d20fafd8dcccd593250e7c4cedda33c6b034a4bb
JinGyeong <wlsrud677@naver.com>
2023-08-08 10:47:41 +0900

    feat : 3pl - 입고 등록 페이지로 이동

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLImportService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLImportService.java
@@ -1,10 +1,14 @@
 package com.shinhan.sunInCloud.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.shinhan.sunInCloud.dto.ImportsDTO;
+import com.shinhan.sunInCloud.dto.ImportProductDTO;
+import com.shinhan.sunInCloud.entity.ImportProduct;
+import com.shinhan.sunInCloud.repository.ImportsProductRepository;
+import com.shinhan.sunInCloud.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,19 +16,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ThreePLImportService {
 	
-	//1.입고예정리스트 -목록 조회
-//	private List<ImportsDTO> seePrelist(Long sellerNo, int pageNum, int countPerPage){
-//		//return importRepo.findImports_
-//	}
-	
-	//2.입고예정리스트 - 상세 조회
+	private final OrderRepository orderRepository;
+	private final ImportsProductRepository importProductRepository;
 	
 	//3.입고 - 등록 패이지로 이동
+	public List<ImportProductDTO> goRegister(Long importNo, int pageNum, int countPerPage, Long sellerNo) {
+		   List<ImportProductDTO> importProductDTOs = new ArrayList<>();
+		   List<ImportProduct> imports = importProductRepository.findByImportNo(importNo);
+		   
+		   for(ImportProduct im: imports) {
+			   importProductDTOs.add(ImportProductDTO.builder().productNo(im.getProduct().getProductNo())
+					   .productName(im.getProduct().getProductName()).importAmount(im.getImportAmount()).
+					   requestAmount(im.getRequestAmount())
+					   .build());
+		   }
+		   
+		   Long count = orderRepository.countBySeller_SellerNo(sellerNo);
+	       Long totalPage = calculatePageCount(count, countPerPage);     
+		   return importProductDTOs;
+	}
 	
 	//4.입고 - 등록
 	
 	//5.입고내역 리스트 -목록 조회
 	
 	//6.입고내역리스트 - 상세 조회
+	
+	private Long calculatePageCount(Long count, int countPerPage) {
+		Long totalPage = count / countPerPage;
+		if (count % countPerPage > 0) {
+			++totalPage;
+		}
+		return totalPage;
+	}
 	
 }
