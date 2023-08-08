b31f2af8f05915713f939f7612116e255b968235
JinGyeong <wlsrud677@naver.com>
2023-08-08 14:17:40 +0900

    feat : 입고 등록 메서드 추가

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLImportService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLImportService.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.shinhan.sunInCloud.dto.ImportProductDTO;
 import com.shinhan.sunInCloud.entity.ImportProduct;
+import com.shinhan.sunInCloud.entity.Imports;
+import com.shinhan.sunInCloud.entity.Product;
+import com.shinhan.sunInCloud.entity.Seller;
 import com.shinhan.sunInCloud.repository.ImportsProductRepository;
+import com.shinhan.sunInCloud.repository.ImportsRepository;
 import com.shinhan.sunInCloud.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,11 +22,14 @@ public class ThreePLImportService {
 	
 	private final OrderRepository orderRepository;
 	private final ImportsProductRepository importProductRepository;
+	private final SellerService sellerService;
+	private final ImportsRepository importRepository;
+	private final ProductService productService;
 	
 	//3.입고 - 등록 패이지로 이동
-	public List<ImportProductDTO> goRegister(Long importNo, int pageNum, int countPerPage, Long sellerNo) {
+	public List<ImportProductDTO> goRegister(Long importNo, int pageNum, int countPerPage) {
 		   List<ImportProductDTO> importProductDTOs = new ArrayList<>();
-		   List<ImportProduct> imports = importProductRepository.findByImportNo(importNo);
+		   List<ImportProduct> imports =  importProductRepository.findByImports_ImportNo(importNo);
 		   
 		   for(ImportProduct im: imports) {
 			   importProductDTOs.add(ImportProductDTO.builder().productNo(im.getProduct().getProductNo())
@@ -31,12 +38,43 @@ public class ThreePLImportService {
 					   .build());
 		   }
 		   
-		   Long count = orderRepository.countBySeller_SellerNo(sellerNo);
+		   Long count = importProductRepository.countByImports_ImportNo(importNo);
 	       Long totalPage = calculatePageCount(count, countPerPage);     
 		   return importProductDTOs;
 	}
 	
 	//4.입고 - 등록
+	   public boolean saveImport(Long sellerNo, List<ImportProductDTO> importProductDTOs) {
+	         // 입고 내역을 저장함 -> 화주사 번호 필요함
+	         Seller seller = sellerService.findById(sellerNo);
+	         Imports imports = importRepository.save(Imports.builder().seller(seller).build());
+	         
+	         //importAmount, importDate가 생김
+	         
+	         // 각 입고 상품에 입고 번호 부여 및 입고 내역과 관계 설정
+	         List<ImportProduct> importProducts = new ArrayList<>();
+	         for (ImportProductDTO importProductDTO : importProductDTOs) {
+	            Product product = productService.findByProductNo(importProductDTO.getProductNo());
+	            ImportProduct importProduct = ImportProduct.builder()
+	                  .imports(imports) // 입고 내역과 관련된 입고 상품 설정
+	                  //.product(product)
+	                  .importAmount(importProductDTO.getImportAmount())
+	                  .build();
+	            importProducts.add(importProduct);
+	         }
+
+	         // 각 입고 상품 저장
+	         importProductRepository.saveAll(importProducts);
+	         
+	         // 입고 내역과 입고 상품이 모두 저장되면 true 반환
+	         boolean importSaved = imports.getImportNo() != null;
+	         
+	         //order테이블에 importNo저장
+//	         if(importSaved) {
+//	        	 orderService.saveImportNoForSeller(orderNo, imports);
+//	         }
+	         return importSaved;
+	      }
 	
 	//5.입고내역 리스트 -목록 조회
 	
