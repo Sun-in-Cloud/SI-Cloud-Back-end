28a485aaf8f96d1b25a2f7f7577e3fc314576403
Eunbeen Oh <svbean77@naver.com>
2023-08-08 12:50:29 +0900

    feat : 3PL 상세 조회

--- SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLService.java
+++ SunInCloud/src/main/java/com/shinhan/sunInCloud/service/ThreePLService.java
@@ -1,14 +1,17 @@
 package com.shinhan.sunInCloud.service;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.shinhan.sunInCloud.entity.ImportProduct;
-import com.shinhan.sunInCloud.entity.Product;
+import com.shinhan.sunInCloud.dto.MatchingDTO;
+import com.shinhan.sunInCloud.dto.ThreePLDTO;
+import com.shinhan.sunInCloud.entity.Matching;
 import com.shinhan.sunInCloud.entity.ThreePL;
-import com.shinhan.sunInCloud.repository.SellerRepository;
 import com.shinhan.sunInCloud.repository.ThreePLRepository;
+import com.shinhan.sunInCloud.util.TimestampUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,7 +20,9 @@ import lombok.RequiredArgsConstructor;
 public class ThreePLService {
 
 	private final ThreePLRepository threePLRepository;
-	private final SellerRepository sellerRepo;
+	private final SellerService sellerService;
+	private final MatchingService matchingService;
+	private final ExportsService exportsService;
 	/**
 	 * 3PL 등록 메소드
 	 * @param threePL
@@ -33,8 +38,34 @@ public class ThreePLService {
 //		return threePLRepository.findByProduct
 //	}
 //	
-	//입고등록에서 입고 등록 수정
+
 	
-	
-	//입고 예정 리스트 
+	/**
+	 * 3PL 정보 상세조회
+	 * @param threePLNo
+	 * @return
+	 */
+	public ThreePLDTO threePLDetail(Long threePLNo) {
+		ThreePL threepl = threePLRepository.findById(threePLNo).orElse(null);
+		List<Matching> matchings = matchingService.findByThreePLNo(threePLNo);
+		List<MatchingDTO> matchingDTOs = new ArrayList<>();
+		long exportCnt = 0;
+		Timestamp endDate = null;
+		
+		for(Matching matching: matchings) {
+			exportCnt += exportsService.getNumberOfSalesYearly(matching.getSeller().getSellerNo(), TimestampUtil.getLastYear() + 1);
+			MatchingDTO matchingDTO = matching.getSeller().toMatchingSellerDTO(matching);
+			matchingDTOs.add(matchingDTO);
+			
+			if(endDate == null) {
+				endDate = matching.getEndDate();
+			} else {
+				if(endDate.compareTo(matching.getEndDate()) > 0) {
+					endDate = matching.getEndDate();
+				}
+			}
+		}
+		
+		return threepl.toThreePLDTO(endDate == null ? null : TimestampUtil.convertTimestampToDate(endDate), exportCnt, matchingDTOs);
+	}
 }
